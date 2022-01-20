cdfa7ba73f3705ba54e1177f61c9cae932d55c80
Ramesh Reddy <vramesh.reddy@gmail.com>
2022-01-20 14:32:41 +0000

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+	let count = 0;
+
+	function handleClick() {
+		count += 1;
+	}
+
+	function resetCounter() {
+		count = 0;
+	}
+</script>
+
+<button on:click={handleClick}>
+	Clicked {count} {count === 1 ? 'time' : 'times'}
+</button>
+
+<button on:click={resetCounter}>Reset Counter</button>
+
+<svelte:options tag="count-btn" />
